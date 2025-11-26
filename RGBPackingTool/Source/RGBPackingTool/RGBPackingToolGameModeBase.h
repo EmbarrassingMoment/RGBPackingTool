2d8be3d0098172ac9dc96c17f9dce0d69d58235e
EmbarrassingMoment <96903403+EmbarrassingMoment@users.noreply.github.com>
2025-11-26 23:19:30 +0900

    First Commit

---
+++ RGBPackingTool/Source/RGBPackingTool/RGBPackingToolGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RGBPackingToolGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RGBPACKINGTOOL_API ARGBPackingToolGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
